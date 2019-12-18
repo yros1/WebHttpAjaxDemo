520d6a3fe6b57bf2efadbfceb2979e077f3bc681
AD\yros <pis2me@hotmail.com>
2019-12-18 21:51:02 +0000

    Add interceptors for response for service calls.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// reister new interceptor
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config before return it.
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit response config before return it.
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
