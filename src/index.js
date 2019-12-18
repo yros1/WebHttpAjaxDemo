55f5577c8564996ebc9b992737390c15e9b02751
AD\yros <pis2me@hotmail.com>
2019-12-18 21:55:37 +0000

    Adding Interceptors to Execute Code Globally

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import axios from 'axios';
 axios.interceptors.request.use(request => {
     console.log(request);
     // Edit request config before return it.
+    // Common use case for it is add some common headers, 
+    // For example Authorisation header.
     return request;
 }, error => {
     console.log(error);
