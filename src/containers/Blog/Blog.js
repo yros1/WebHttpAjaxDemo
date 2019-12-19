66c2f75f71c3129d2316c3731e7ccc20e270cf89
yros1 <31065715+yros1@users.noreply.github.com>
2019-12-19 09:32:05 +0000

    Creating and Using Axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios'; // this import points to axios instnce created in axios.js file
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
