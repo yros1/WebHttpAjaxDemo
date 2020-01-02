eccd81038af1ab2fb9ee314985cc75bd9cdc8a30
AD\yros <pis2me@hotmail.com>
2020-01-02 19:46:54 +0000

    Setting Up and Rendering Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -13,10 +16,19 @@ class Blog extends Component {
                         <ul>
                             <li><a href="/">Home</a></li>
                             <li><a href="/new-post">New Post</a></li>
+                            <li><a href="/full-post">Full Post</a></li>
+                            
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* localhost:3000/ display <Posts /> */}
+                <Route path="/" exact render={() => <Posts />} />
+                
+                {/* localhost:3000/ display <FullPost /> */}
+                <Route path="/new-post" render={() => <FullPost />} />
+                
+                {/* localhost:3000/ display <NewPost /> */}
+                <Route path="/full-post" render={() => <NewPost />} />
             </div>
         );
     }
